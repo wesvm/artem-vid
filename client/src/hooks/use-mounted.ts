08d5aad44c2d308d424456ac63cfae09a472a37e
blue <samuel_walter_@hotmail.com>
2025-04-09 18:28:22 -0500

    feat(client): add Drawer and Badge components with hooks

---
+++ client/src/hooks/use-mounted.ts
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export function useIsMounted() {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+
+    return () => setMounted(false);
+  }, []);
+
+  return mounted;
+}
